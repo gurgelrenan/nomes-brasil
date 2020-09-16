68bacbdd87f32d513f25fb983322bf217de04ab0
Renan Gurgel <gurgel.renan@gmail.com>
2020-09-15 23:55:51 -0300

    First version of basic data show in the home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import { averageIncomes } from './data';
+
+const App = () => {
+  const [uf, setUf] = useState('');
+  const [salary, setsalary] = useState(0);
+
+  const handleChangeSalary = event => {
+    const { value } = event.target;
+    setsalary(value);
+  }
+
+  const handleChangeUf = event => {
+    const { value } = event.target;
+    setUf(value);
+  }
+
+  const getSalaryByUf = (params_uf) => {
+    if (params_uf) {
+      return averageIncomes.filter((idx) => idx.uf === params_uf )[0].value;  
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home'>
+      <h2>Compare seu salário com a média nacional.</h2>
+      <form>
+        <label>Salário</label>
+        <input type="text" onChange={handleChangeSalary} value={salary} />
+
+        <label>Estado</label>
+        <select value={uf} onChange={handleChangeUf}>
+          {
+            averageIncomes.map(
+              ({uf, value}) => (
+              <option value={uf}>{uf}</option>
+              )
+            )
+          }
+        </select>
+      </form>
+
+      <span>Seu salário R$ {salary}</span><br />
+        <span>A média salarial no estado de {uf} é: R$ {getSalaryByUf(uf)}</span>
+
     </div>
   );
 }
