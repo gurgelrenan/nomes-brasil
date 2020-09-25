c013e825249d7cc70176ccdbdcb3ad3917cec068
Renan Gurgel <gurgel.renan@gmail.com>
2020-09-24 22:34:47 -0300

    Finish basic style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
 import { averageIncomes } from './data';
 import SalaryTable from './components/salary-table.component';
 
-const App = () => {
+function Copyright() {
   return (
-    <SalaryTable props={averageIncomes} />
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        @gurgelrenan
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Média Salarial Brasileira
+        </Typography>
+        <SalaryTable props={averageIncomes} />
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
